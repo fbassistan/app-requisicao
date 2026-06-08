ec108aa66b410d7a0a524b7b356cd21a4fa0d4d4
fbassistan <fb.assistant@thebarracuda.com.br>
2026-06-08 12:57:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,19 @@ import urllib.request
 import json
 import time
 import re
+import unicodedata # ➔ Biblioteca mágica para remover acentos
 
 st.set_page_config(page_title="Requisição Diária", page_icon="📝", layout="centered")
 
 # ➔ SUA URL DO APP DA WEB DO GOOGLE SCRIPTS (Terminada em /exec)
 URL_WEB_APP = "https://script.google.com/macros/s/AKfycbzcNped3ftP-9FkLcWC-u65kl0RlX-rW2Z_8AHLGKgrw2ETjkoKJI2CHqisiSQnoUUb/exec"
 
-# Banco de Dados Geral e Auditado (Cozinha, Bar e Salão)
+# Banco de Dados Geral e Auditado (202 Itens Cozinha | 169 Itens Bar | 81 Itens Salão)
 DADOS_SISTEMA = {
     "RESTAURANTE / COZINHA": {
         "CARNES / AVES": ["COSTELA BOI KG", "FILÉ MIGNON PEÇA KG", "HAMBURGER BOVINO 160 G", "CUPIM KG", "COXA SOBRECOXA KG", "FILE FRANGO KG", "RABADA KG", "MOCOTÔ KG", "PICANHA PEÇA", "BIFE ANCHO KG"],
         "PEIXES / FRUTOS DO MAR": ["FILÉ DE CAMARÃO TAINHA UND", "CAMARAO VG FILE PORÇÃO", "FILÉ DE PEIXE UND", "FILÉ DE ATUM UND", "LAGOSTA PARA 01 PESSOA UND", "LAGOSTA PARA 02 PESSOA UND", "PEIXE MEDIO INTEIRO UND", "PEIXE PARA MOQUECA UND", "CATADO SIRI KG", "CAMARAO ROSA KG", "POLVO KG"],
-        "FRIOS / LATICINIOS": ["BACON KG", "CREAM CHEcheese 150G UND", "LEITE LEITÍSSIMO (L)", "MANTEIGA 500G", "MANTEIGA DE GARRAFA (L)", "PRESUNTO PARMA KG", "QUEIJO BRANCO M. FRESCAL KG", "QUEIJO COALHO KG", "QUEIJO GORGONZOLA KG", "QUEIJO GRANA PADANO 250G", "QUEIJO MUSSARELA KG", "QUEIJO PARMESÃO KG", "REQUEIJÃO CREMOSO UND"],
+        "FRIOS / LATICINIOS": ["BACON KG", "CREAM CHEESE 150G UND", "LEITE LEITÍSSIMO (L)", "MANTEIGA 500G", "MANTEIGA DE GARRAFA (L)", "PRESUNTO PARMA KG", "QUEIJO BRANCO M. FRESCAL KG", "QUEIJO COALHO KG", "QUEIJO GORGONZOLA KG", "QUEIJO GRANA PADANO 250G", "QUEIJO MUSSARELA KG", "QUEIJO PARMESÃO KG", "REQUEIJÃO CREMOSO UND"],
         "MANTIMENTOS SECOS": ["ACUCAR REFINADO UNIAO", "AÇÚCAR MASCAVO", "AMEIXA SECA KG", "AMENDOA CACAU S/CASCA KG", "AMIDO DE MILHO", "ARROZ BRANCO KG", "ARROZ NEGRO", "ARROZ VERMELHO ORIENTAL", "BISCOITO DE MAIZENA", "CAFÉ EM PÓ 500G", "CANJIQUINHA MILHO BRANCO 200G", "CASTANHA DO PARÁ", "SEMENTE DE CHIA", "CHOCOLATE AMMA BARRA 500G", "CHOCOLATE BRANCO", "COUSCOUS MARROQUINO (KG)", "FARINHA DE MANDIOCA KG", "FARINHA TAPIOCA GRANULADA", "FARINHA DE TRIGO", "FARINHA DE TRIGO INTEGRAL", "FARINHA PANKO", "FEIJAO FRADINHO SEM CASCA", "FEIJÃO PRETO", "FERMENTO BIOLÓGICO", "FERMENTO EM PÓ QUIMICO", "FLOCÃO DE MILHO", "FLOR DE SAL", "FUBÁ DE MILHO", "GERGELIM BRANCO", "GERGELIM NEGRO", "GRÃO DE BICO KG", "LEITE EM PÓ 200G", "LINHAÇA DOURADA", "MASSA TAGLIATELLE RISCOSSA", "NOZES", "PÁPRICA", "QUINOA GRÃOS", "SÊMOLA GRANO DURO 500GR", "SPAGHETTI SEM GLÚTEN", "UVA PASSA", "XAROPE DE GLUCOSE", "PASTA DE AMENDOIM", "PASTA BAUNILHA"],
         "HORTIFRUTI / TEMPEROS": ["ABACATE UND", "ABACAXI UND", "ABOBORA KG", "ABOBRINHA KG", "AGRIÃO UND", "AIPIM KG", "POLPA DE FRUTAS KG", "ALECRIM UND", "ALECRIM DO MATO", "ALFACE AMERICANA", "ALFACE CRESPA UND", "ALFACE ROXA", "ALHO KG", "ALHO PORÓ UND", "BANANA DA PRATA UND", "BANANA DA TERRA UND", "BATATA BAROA", "BATATA DOCE KG", "BATATA INGLESA KG", "BERINGELA KG", "BETERRABA KG", "BRÓCOLIS JAPONÊS UND", "CACAU VERDE UND", "CAJÚ UND", "CAPIM SANTO UND", "CEBOLA BRANCA KG", "AÇAFRÃO EM PÓ KG", "ACETO BALSÂMICO", "ALCAPARRAS KG", "ALCAPARRONES 180G", "AZEITE DENDÊ (L)", "AZEITE EXTRA VIRGEM ANDORINHA", "AZEITE TRUFADO", "AZEITONA PRETA SEM CAROÇO", "AZEITONA VERDE SEM CAROÇO", "CANELA EM PÓ", "CASTANHA DE CAJU", "CATCHUP 395G", "COGUMELO CHAMPIGNON FRESCO", "COGUMELO SHIMEJI FRESCO", "COGUMELO SHITAKE FRESCO", "CREME DE LEITE", "EXTRATO DE TOMATE", "FOLHA DE LOURO", "FOLHA DE ALGAS", "FOLHA DE ARROZ", "GELATINA SEM SABOR", "LEITE CONDENSADO", "LEITE CONDENSADO MOÇA LATA", "LEITE DE COCO", "MILHO VERDE ESPIGA", "MOLHO AJI NO SHOYU", "MOLHO TABASCO", "MOSTARDA DJON 1KG", "ÓLEO DE COCO", "ÓLEO DE GERGELIN", "ÓLEO DE GIRASSOL", "ÓLEO DE SOJA 900ML", "ORÉGANO", "PALMITO FRESCO SEM CASCA", "PIMENTA CAIENA KG", "PIMENTA CALABRESA KG", "SAL GROSSO", "SAL MOÍDO KG", "TAHINE 330G", "TOMATE PELADO 400G", "VINAGRE DE CEREAL ARROZ UND", "VINHO BRANCO COZINHA", "VINHO TINTO COZINHA", "MELACO DE CANA", "BICARBONATO DE SODIO", "HORTELÃ", "LARANJA PERA UND", "LIMÃO TAHITI KG", "LIMÃO SICILIANO KG", "MAÇÃ VERDE KG", "MAMÃO KG", "MANGA KG", "MANJERICÃO", "MARACUJÁ KG", "MAXIXE", "MELANCIA UND", "OVO BRANCO/VERMELHO UND", "OVO DE GALINHA CAIPIRA UND", "PEPINO KG", "PIMENTA ARDIDA KG", "PIMENTA DOCE VERMELHA KG", "QUIABO KG", "RABANETE KG", "REPOLHO ROXO", "RÚCULA UND", "SALSA UND", "SALSÃO UND", "TOMATE KG", "TOMATE CEREJA KG", "TOMILHO", "VAGEM", "PEPINO JAPONES", "CANELA EM PAU", "CEBOLA ROXA KG", "CEBOLINHA UND", "CENOURA KG", "COCO SECO UND", "COENTRO UND", "COUVE UND", "COUVE FLOR UND", "ERVA DOCE", "ESPINAFRE", "FEIJÃO VERDE KG", "FLORES COMESTÍVEIS", "FOLHA DE VINAGREIRA", "GENGIBRE KG"],
         "DIVERSOS": ["PAPEL ALUMÍNIO", "PAPEL TOALHA INSTIT. C/ 06 UND", "BOBINA P ALIMENTOS", "MASCARA DESCARTAVEL DOBRAVEL", "SACO DE LIXO 200L", "FIBRACO SCOTCH BRITE", "FILME PVC 400MT", "ESCOVA DE ACO", "LAVA LOUCAS CONC. P. DILUIR 500ML", "DESENGORDURANTE ALCALINO CHEF", "ESPONJA DUPLA FACE", "AVENTAL BRANCO EM PVC", "CARVAO", "PAPEL MANTEIGA", "GAS PARA MASSARICO UND"]
@@ -34,6 +35,10 @@ DADOS_SISTEMA = {
         "CONSUMÍVEIS / DIVERSOS": ["SORO FISIOLOGICO", "ALGODÃO", "BAND-AID", "CANETA ESFEROGRÁFICA", "CANETA PILOTO AZUL", "CANETA PILOTO PRETA", "CANELA PILOTO VERMELHO", "ESPARADRAPO", "GASE", "GUARDANAPO 40 X 40", "PAPEL RECICLADO TAMANHO A4", "PILHA AA", "PILHA AAA", "PISTOLA DE ISQUEIRO", "REPELENTE LOÇÃO", "REPELENTE OFF SPRAY", "VELAS DE ANIVERSÁRIO", "VELAS GRANDES 10X15", "VELAS MÉDIAS 5,5X15", "VELAS PEQUENAS 3X6", "ABACAXI VERDE", "AMENDOIM 500G", "AZEITE EXTRA VIRGEM EA 500ML", "AZEITONAS PRETAS", "CACHO DE BANANA VERDE", "CASTANHA DE CAJÚ", "COCO SECO", "COCO VERDE", "FLOR DE SAL", "GRAVIOLA (ESCREVER A FRUTA)", "SAL GROSSO", "SAL REFINADO"]
     }
 }
+
+# ➔ FUNÇÃO DE LIMPEZA DE ACENTOS: transforma "ÁGUA" em "AGUA" apenas para comparação
+def remover_acentos(texto):
+    return "".join(c for c in unicodedata.normalize('NFD', str(texto)) if unicodedata.category(c) != 'Mn').upper().strip()
 
 def separar_unidade(texto_item):
     texto = texto_item.strip().upper()
@@ -63,6 +68,7 @@ setor_selecionado = st.selectbox("Selecione o seu Setor:", list(DADOS_SISTEMA.ke
 lista_itens_plana = []
 for subcat, itens in DADOS_SISTEMA[setor_selecionado].items():
     lista_itens_plana.extend(itens)
+lista_itens_plana = sorted(list(set(lista_itens_plana)))
 
 st.write("---")
 
@@ -74,16 +80,36 @@ if item_nao_cadastrado:
     item_nome_limpo = item_bruto.upper().strip()
     subcategoria_detectada = "MANUAL / NOVO ITEM"
 else:
-    item_bruto = st.selectbox("Busque e selecione o Item:", sorted(list(set(lista_itens_plana))), key=f"item_select_{st.session_state.reset_counter}")
-    item_nome_limpo, unidade_medida = separar_unidade(item_bruto)
+    # ➔ NOVA BARRA DE BUSCA BLINDADA CONTRA ACENTOS:
+    texto_busca = st.text_input("🔍 Digite para buscar o item (pode escrever sem acentos):", placeholder="Ex: agua, file, maca, suco...", key=f"busca_{st.session_state.reset_counter}")
     
-    subcategoria_detectada = "OUTROS"
-    for subcat, itens in DADOS_SISTEMA[setor_selecionado].items():
-        if item_bruto in itens:
-            subcategoria_detectada = subcat
-            break
+    # Filtra a lista comparando os textos sem acentos de ambos os lados
+    termo_filtrado = remover_acentos(texto_busca)
+    if termo_filtrado:
+        lista_filtrada = [item for item in lista_itens_plana if termo_filtrado in remover_acentos(item)]
+    else:
+        lista_filtrada = lista_itens_plana
 
-st.info(f"⚖️ Unidade de medida: **{unidade_medida}**")
+    # Se a busca não encontrar nada, avisa o usuário de forma amigável
+    if not lista_filtrada:
+        st.warning("❌ Nenhum item encontrado com esse nome. Verifique a digitação ou marque a caixa acima para inserir manualmente.")
+        item_nome_limpo = ""
+        unidade_medida = "UND"
+        subcategoria_detectada = "OUTROS"
+    else:
+        item_bruto = st.selectbox("Selecione o Item na lista:", lista_filtrada, key=f"item_select_{st.session_state.reset_counter}")
+        item_nome_limpo, unidade_medida = separar_unidade(item_bruto)
+        
+        subcategoria_detectada = "OUTROS"
+        for subcat, itens in DADOS_SISTEMA[setor_selecionado].items():
+            if item_bruto in itens:
+                subcategoria_detectada = subcat
+                break
+
+if not item_nao_cadastrado and lista_filtrada:
+    st.info(f"⚖️ Unidade de medida detectada: **{unidade_medida}**")
+elif item_nao_cadastrado:
+    st.info(f"⚖️ Unidade de medida selecionada: **{unidade_medida}**")
 
 quantidade = st.number_input(f"Quantidade necessária:", min_value=1, value=1, step=1, key=f"qtd_{st.session_state.reset_counter}")
 observacao = st.text_input("Observação (Opcional):", placeholder="Ex: Urgente, Marca específica...", key=f"obs_{st.session_state.reset_counter}")
@@ -92,7 +118,7 @@ if st.button("➕ Adicionar Item ao Pedido", use_container_width=True):
     if nome_solicitante.strip() == "":
         st.error("Por favor, preencha o seu nome antes de adicionar itens.")
     elif item_nome_limpo == "":
-        st.error("Por favor, selecione ou digite o nome do item.")
+        st.error("Por favor, selecione ou digite um item válido antes de adicionar.")
     else:
         texto_obs = observacao.strip() if observacao.strip() != "" else "-"
         
@@ -142,7 +168,7 @@ if not st.session_state.carrinho_df.empty:
     
     if st.button("🚀 ENVIAR REQUISIÇÃO PARA A CENTRAL", type="primary", use_container_width=True):
         if "COLE_AQUI" in URL_WEB_APP:
-            st.error("Erro: Falta colocar a sua URL do Google Script na linha 12!")
+            st.error("Erro: Falta colocar a sua URL do Google Script na linha 13!")
         else:
             with st.spinner("Enviando dados diretamente para o Google Sheets..."):
                 try:
